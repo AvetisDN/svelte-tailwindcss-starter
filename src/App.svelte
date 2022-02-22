ba49d4fe770789535a322ce917a7426ce3ea656f
AvetisDN <avetisdn@gmail.com>
2022-02-22 17:12:16 +0300

    svelte + tailwindcss + Inter + FontAwesome6

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let name = 'World';
+</script>
+
+<main>
+	<h1 class="text-6xl font-bold text-rose-500">
+		Hello {name}
+		<i class="fa-solid fa-bacteria"></i>
+	</h1>
+</main>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
